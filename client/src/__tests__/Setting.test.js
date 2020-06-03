0afe6a10fa3ed88906921a1e19ac3a9496ba05b4
Troemer16 <troemer@mail.greenriver.edu>
2020-06-03 16:13:57 -0700

    Finished testing the setting components use of its props when settings are changed

--- client/src/__tests__/Setting.test.js
+++ client/src/__tests__/Setting.test.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import Setting from '../components/Setting';
+import Settings from '../components/Setting';
 
-test('renders speed settings', () => {
-    const wrapper = shallow(<Setting/>);
-    
-    //Div containing speed settings with adjuster and label
-    expect(wrapper.find('.Game_settings')).toHaveLength(1);
-    expect(wrapper.find('#speed-slider')).toHaveLength(1);
-    expect(wrapper.find('#speed-label')).toHaveLength(1);
+describe('renders all setting', () => {
+    test('renders speed settings', () => {
+        const wrapper = shallow(<Setting/>);
+        
+        //Div containing speed settings with adjuster and label
+        expect(wrapper.find('.Game_settings')).toHaveLength(1);
+        expect(wrapper.find('#speed-slider')).toHaveLength(1);
+        expect(wrapper.find('#speed-label')).toHaveLength(1);
+    });
+
+    test('renders open border settings', () => {
+        const wrapper = shallow(<Setting/>);
+        
+        //Div containing speed settings with adjuster and label
+        expect(wrapper.find('.Border_setting')).toHaveLength(1);
+        expect(wrapper.find('#open-border')).toHaveLength(1);
+        expect(wrapper.find('#open-border-label')).toHaveLength(1);
+    });
+});
+
+describe('changing settings call appropriate prop functions', () => {
+    test('changing speed calls setSpeed method from props', () => {
+        const mockedFunction = jest.fn();
+        const wrapper = shallow(<Settings setSpeed = {mockedFunction}/>);
+
+        const speed = wrapper.find('#speed-slider');
+        speed.simulate('changeCommitted', {target: {value: 0.5}});
+
+        expect(mockedFunction).toHaveBeenCalledTimes(1);
+    });
+
+    test('clicking open border checkbox calls setBorder method from props', () => {
+        const mockedFunction = jest.fn();
+        const wrapper = shallow(<Settings setBorder = {mockedFunction}/>);
+
+        const border = wrapper.find('#open-border');
+        border.simulate('change', {target: {value: 1}});
+
+        expect(mockedFunction).toHaveBeenCalledTimes(1);
+    });
 });
